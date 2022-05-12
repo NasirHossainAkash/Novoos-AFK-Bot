3c1988beecca7f46757d9de727eaa6786eede5f4
Nasir Hossain Akash <nasirhossainakash5@gmail.com>
2022-05-12 17:51:43 +0600

    added last seen support

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Python 3.10.4
 
 
-
+import datetime
 import redis
 from os import getenv
 from pyrogram import Client, filters
@@ -13,10 +13,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-
 load_dotenv('config.env')
-
-
 
 
 
@@ -27,6 +24,7 @@ REDIS_URI = getenv("REDIS_URI")
 REDIS_PASSWORD = getenv("REDIS_PASSWORD")
 REDIS_URL = str(REDIS_URI).split(":")[0]
 REDIS_PORT = str(REDIS_URI).split(":")[1]
+
 
 r = redis.Redis(
     host=REDIS_URL,
@@ -43,9 +41,11 @@ def is_afk():
     else:
         return False
 
+
 def get_reason():
     reason = str(r.get(name="REASON"),"UTF-8")
     return reason
+
 
 def set_reason(reason):
     r.set(name="REASON",value=reason)
@@ -53,26 +53,43 @@ def set_reason(reason):
 
 @app.on_message(filters.me & filters.regex('^.afk'))
 async def set_afk(client, message):
+    global start_time
     reason = message.text.replace('.afk', """""").strip()
     r.set(name="AFK",value="True")
     set_reason(reason=reason)
+    start_time = datetime.datetime.now()
     await message.edit(f"__Going AFK__\n__Reason: {reason}__\n\n__Made with ❤️ by__\n**__@NovoosEcosystem__**")
     await asyncio.sleep(2)
     await message.delete()
+
 
 @app.on_message(filters.me & filters.text)
 async def disable(client, message):
     r.set(name="AFK", value="False")
 
 
-
 @app.on_message(filters.private)
 async def afk(client, message):
     if is_afk():
         reason = get_reason()
-        await message.reply(f"__Hello {message.chat.first_name}__\n__Currently I am AFK__\n__Reason: {reason}__\n\n**__Made with ❤️ by__**\n**__@NovoosEcosystem__** ")
-
-
+        finished_time = datetime.datetime.now()
+        total_afk_time = finished_time - start_time
+        time = int(total_afk_time.seconds)
+        d = time // (24 * 3600)
+        time %= 24 * 3600
+        h = time // 3600
+        time %= 3600
+        m = time // 60
+        time %= 60
+        s = time
+        endtime = ""
+        if d > 0:
+            endtime += f"{d}d {h}h {m}m {s}s"
+        elif h > 0:
+            endtime += f"{h}h {m}m {s}s"
+        else:
+            endtime += f"{m}m {s}s" if m > 0 else f"{s}s"
+        await message.reply(f"__Hello {message.chat.first_name}__\n__Currently I am AFK__\n__Reason: {reason}__\n__Last Seen: {endtime} ago__\n**__Made with ❤️ by__**\n**__@NovoosEcosystem__** ")
 
 
 app.run()
